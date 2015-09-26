9492fc96ad8949908d16f4e7cd7df34d9ba236b2
guptakvgaurav <guptakvgaurav@gmail.com>
2015-09-26 14:55:10 +0530

    Fixed js for production

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var app            = express();
 
 app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
 
-app.listen(8000, '127.0.0.1');   
+app.listen(8080, '127.0.0.1');   
